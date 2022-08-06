df21d378ab71dd9159d060fa3997108f37789836
mehmeturmac <mehmeturmac@gmail.com>
2022-08-06 20:55:40 +0300

    tailwind initialized and header added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="relative bg-white">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-7xl mx-auto px-4 sm:px-6">
+      <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+          <a href="/">
+            <span className="sr-only">BMI</span>
+            <img className="h-8 w-auto sm:h-10" src="https://cdn-icons-png.flaticon.com/512/3373/3373123.png" alt="" />
+          </a>
+        </div>
+        <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+          Calculator
+        </a>
+        <a href="/whatisbmi" className="text-base font-medium text-gray-500 hover:text-gray-900">
+          What is BMI?
+        </a>
+        <a href="/dietlist" className="text-base font-medium text-gray-500 hover:text-gray-900">
+          Diet List
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
